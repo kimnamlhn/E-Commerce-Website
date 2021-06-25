f1cb4fa73dcd1e9e6a18da16dd2bf81e48a9170f
kimnamlhn <kimnam.cpc@gmail.com>
2021-06-25 09:58:26 +0700

    layout web

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
+var exphbs  = require('express-handlebars');
+
 const app = express()
 const port = 3000
- 
+
+
+app.engine('hbs', exphbs(
+  {
+    extname: '.hbs',
+    layoutsDir: './views/_layout'
+  }
+));
+app.set('view engine', 'hbs');
+  
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
+  res.render('home');
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   })
+
+
+  
---
+++ views/middlewares/view.mdw.js
@@ -0,0 +1,18 @@
+var exphbs  = require('express-handlebars');
+var express_handlebars_sections = require('express-handlebars-sections');
+
+
+module.exports = function (app){
+    app.engine('hbs', exphbs({
+        defaultLayout: 'main.hbs',
+        extname: '.hbs',
+        layoutsDir: 'views/_layouts',
+        partialsDir: 'views/_partials',
+        helpers: {
+          section: express_handlebars_sections(),
+        },
+      }
+      ));
+      app.set('view engine', 'hbs');
+}
+
