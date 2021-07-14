ac0fb900081f669467b47e86b52729a89fa78c92
kimnamlhn <kimnam.cpc@gmail.com>
2021-07-14 18:01:01 +0700

    user pages

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,6 +5,21 @@ router.get('/', async function (req, res) {
     res.render('user/profile');
   })
 
+// danh sách sản phẩm đã đặt và tình trạng
+router.get('/order_placed', async function (req, res) {
+    res.render('user/order_placed');
+  })
+
+// lịch sử đặt hàng
+router.get('/purchase_history', async function (req, res) {
+    res.render('user/purchase_history');
+  })
+
+
+router.get('/cart', async function (req, res) {
+    res.render('user/purchase_history');
+  })
+
 
 
 module.exports = router;
